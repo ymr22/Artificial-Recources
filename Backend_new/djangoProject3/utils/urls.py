2d665a781b696e944ace8bb16a02e394fc204359
nnilayd <84875785+nnilayd@users.noreply.github.com>
2024-03-28 23:36:34 +0300

    cv post method implemented

--- Backend_new/djangoProject3/utils/urls.py
+++ Backend_new/djangoProject3/utils/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('cvinfo/', views.cv_info, name='cv_info'),
     path('cvscan/', views.cv_scan, name='cv_scan'),
+    path('cvcommit/', views.cv_commit, name='cv_commit'),
     path('offday/', views.off_day, name='off_day'),
+    path('csrftoken/', views.get_csrf_token, name='csrf'),
 ]
