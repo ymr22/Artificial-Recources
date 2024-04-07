644179040ed7d0330d7ff67f09e09fae124c0dde
nnilayd <84875785+nnilayd@users.noreply.github.com>
2024-04-07 17:34:51 +0300

    change password update

--- Backend_new/djangoProject3/user/serializers.py
+++ Backend_new/djangoProject3/user/serializers.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import authenticate
+from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers, generics
 from django.contrib.auth.models import User
 
@@ -55,30 +56,15 @@ class UserSerializer(serializers.ModelSerializer):
             'last_name',
         ]
 
-class ChangePasswordSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
-    password2 = serializers.CharField(write_only=True, required=True)
-    old_password = serializers.CharField(write_only=True, required=True)
+class ChangePasswordSerializer(serializers.Serializer):
+    model = User
 
-    class Meta:
-        model = User
-        fields = ('old_password', 'password', 'password2')
+    """
+    Serializer for password change endpoint.
+    """
+    old_password = serializers.CharField(required=True)
+    new_password = serializers.CharField(required=True)
 
-    def validate(self, attrs):
-        if attrs['password'] != attrs['password2']:
-            raise serializers.ValidationError({"password": "Password fields didn't match."})
-
-        return attrs
-
-    def validate_old_password(self, value):
-        user = self.context['request'].user
-        if not user.check_password(value):
-            raise serializers.ValidationError({"old_password": "Old password is not correct"})
-        return value
-
-    def update(self, validated_data):
-
-        self.context['request'].user.set_password(validated_data['password'])
-        self.context['request'].user.save()
-
-        return self.context['request'].user
+    # def validate_new_password(self, value):
+    #     validate_password(value)
+    #     return value
--- Backend_new/djangoProject3/user/views.py
+++ Backend_new/djangoProject3/user/views.py
@@ -2,7 +2,10 @@ from django.contrib.auth import login, logout
 from django.contrib.auth.models import User
 from rest_framework import permissions, status, generics
 from rest_framework import views
+from rest_framework.generics import UpdateAPIView
 from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from . import serializers
 from rest_framework.permissions import IsAuthenticated
 
@@ -36,8 +39,62 @@ class ProfileView(generics.RetrieveAPIView):
         return self.request.user
 
 
-class ChangePasswordView(generics.UpdateAPIView):
-
-    queryset = User.objects.all()
-    permission_classes = (IsAuthenticated,)
+class ChangePasswordView(UpdateAPIView):
+    """
+    An endpoint for changing password.
+    """
     serializer_class = ChangePasswordSerializer
+    model = User
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self, queryset=None):
+        obj = self.request.user
+        return obj
+
+    def update(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        serializer = self.get_serializer(data=request.data)
+
+        if serializer.is_valid():
+            # Check old password
+            if not self.object.check_password(serializer.data.get("old_password")):
+                return Response({"old_password": ["Wrong password."]}, status=status.HTTP_400_BAD_REQUEST)
+            # set_password also hashes the password that the user will get
+            self.object.set_password(serializer.data.get("new_password"))
+            self.object.save()
+            response = {
+                'status': 'success',
+                'code': status.HTTP_200_OK,
+                'message': 'Password updated successfully',
+                'data': []
+            }
+
+            return Response(response)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    # class UpdatePassword(APIView):
+    #     """
+    #     An endpoint for changing password.
+    #     """
+    #     permission_classes = (permissions.IsAuthenticated,)
+    #
+    #     def get_object(self, queryset=None):
+    #         return self.request.user
+    #
+    #     def put(self, request, *args, **kwargs):
+    #         self.object = self.get_object()
+    #         serializer = ChangePasswordSerializer(data=request.data)
+    #
+    #         if serializer.is_valid():
+    #             # Check old password
+    #             old_password = serializer.data.get("old_password")
+    #             if not self.object.check_password(old_password):
+    #                 return Response({"old_password": ["Wrong password."]},
+    #                                 status=status.HTTP_400_BAD_REQUEST)
+    #             # set_password also hashes the password that the user will get
+    #             self.object.set_password(serializer.data.get("new_password"))
+    #             self.object.save()
+    #             return Response(status=status.HTTP_204_NO_CONTENT)
+    #
+    #         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
