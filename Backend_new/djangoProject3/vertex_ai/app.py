1698fd0bc169c88deb44c5f37fe2e3d3227639f8
nnilayd <84875785+nnilayd@users.noreply.github.com>
2024-04-09 19:22:46 +0300

    calendar predict function added

--- Backend_new/djangoProject3/vertex_ai/app.py
+++ Backend_new/djangoProject3/vertex_ai/app.py
@@ -38,3 +38,25 @@ def offDay_model_predict(query, used_off_days):
         **parameters
     )
     return response.text
+
+
+def calendar_predict(query):
+    vertexai.init(project="267233172569", location="europe-west2")
+    parameters = {
+        "candidate_count": 1,
+        "max_output_tokens": 1024,
+        "temperature": 0.2,
+        "top_p": 0.8,
+        "top_k": 40
+    }
+    model = TextGenerationModel.from_pretrained("text-bison@001")
+    model = model.get_tuned_model("projects/267233172569/locations/europe-west2/models/2155843234905980928")
+    response = model.predict(
+        """Zehra Koç pazartesi saat 9:00 da nerede olma ihtimali yüksek""",
+        **parameters
+    )
+    print(f"Response from Model: {response.text}")
+    return response.text
+
+
+
